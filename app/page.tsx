6c31f7caa9fdd2650190f0937240d41cbd42be29
João Marcelo Dantas <jmgrd98@gmail.com>
2023-07-28 16:24:12 -0300

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,34 @@
 
 import Image from 'next/image'
 import Header from "@/components/Header";
+import image from '../public/assets/images/image.png'
 
 export default function Home() {
-  return (
-    <section className='p-10 flex flex-col gap-5 w-1/2'>
-        <h1 className='font-bold text-yellow-500 text-5xl'>Lorem ipsum dolor sit amet, consectetur</h1>
-        <p className='text-gray-600 text-2xl'>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor</p>
+    return (
+        <section className='p-10 flex gap-5 w-full'>
 
-      <button className='bg-yellow-500 p-4 text-white font-bold rounded-3xl w-1/2'>SIGN UP NOW -></button>
+            <section className='mt-20 flex flex-col gap-5 w-1/2'>
+                <h1 className='font-bold text-yellow-500 text-5xl'>Lorem ipsum dolor sit amet, consectetur</h1>
+                <p className='text-gray-600 text-2xl'>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
+                    aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor</p>
 
-        <div className='flex gap-5'>
+                <button className='bg-yellow-500 p-4 text-white font-bold rounded-3xl w-1/2'>SIGN UP NOW -></button>
 
-          <span className='bg-opacity-20 rounded bg-yellow-500 text-yellow-500 p-3'>Cryptos</span>
-          <span className='bg-opacity-20 rounded bg-yellow-500 text-yellow-500 p-3'>NFTs</span>
-          <span className='bg-opacity-20 rounded bg-yellow-500 text-yellow-500 p-3'>Games</span>
+                <div className='flex gap-5'>
 
-        </div>
-    </section>
-  )
+                    <span className='bg-opacity-20 rounded bg-yellow-500 text-yellow-500 p-3'>Cryptos</span>
+                    <span className='bg-opacity-20 rounded bg-yellow-500 text-yellow-500 p-3'>NFTs</span>
+                    <span className='bg-opacity-20 rounded bg-yellow-500 text-yellow-500 p-3'>Games</span>
+
+                </div>
+
+            </section>
+
+            <section className='w-1/2'>
+                <Image src={image} alt='Woman image' width={1000} height={1000}/>
+            </section>
+
+
+        </section>
+    )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link'
 import Image from 'next/image'
-import { useState, useEffect } from 'react'
+import {useState, useEffect} from 'react'
 import '../app/globals.css'
 import logo from '../public/assets/images/logo.png'
 import axios from "axios";
@@ -12,16 +12,32 @@ export default function Header() {
     const apiKey: string = 'D755543C-00D2-465A-B026-D3D5E576CF90';
     const baseUrl: string = 'https://rest.coinapi.io/v1';
 
+    const [solanaPrice, setSolanaPrice] = useState(0);
+    const [xlmPrice, setXlmPrice] = useState(0);
+
     async function fetchCoinData() {
-        try{
+        try {
             const response = await axios.get(`${baseUrl}/exchangerate/SOL/BRL`, {
                 headers: {
                     'X-CoinAPI-Key': apiKey
                 }
             });
 
-            const solanaPrice = response.data.rate;
+            setSolanaPrice(response.data.rate);
             console.log('SOL price: ', solanaPrice);
+        } catch (error) {
+            console.error(error);
+        }
+
+        try {
+            const response = await axios.get(`${baseUrl}/exchangerate/XLM/BRL`, {
+                headers: {
+                    'X-CoinAPI-Key': apiKey
+                }
+            });
+
+            setXlmPrice(response.data.rate);
+            console.log('XLM price: ', xlmPrice);
         } catch (error) {
             console.error(error);
         }
@@ -33,31 +49,50 @@ export default function Header() {
 
 
     return (
-        <header className="flex items-center justify-between w-full h-16 px-4 bg-white border-b border-gray-200 dark:bg-zinc-800 dark:border-zinc-700">
+        <header
+            className="flex items-center justify-between w-full h-16 px-4 bg-white border-b border-gray-200 dark:bg-zinc-800 dark:border-zinc-700">
 
             <div className="flex items-center justify-between w-full max-w-5xl">
                 <div className="flex items-center">
                     <a href="/" className="flex items-center justify-between">
-                        <h1 className="ml-2 text-xl font-bold text-yellow-500">Coin<span className='text-gray-500'>Synch</span></h1>
+                        <h1 className="ml-2 text-xl font-bold text-yellow-500">Coin<span
+                            className='text-gray-500'>Synch</span></h1>
                         <Image src={logo} alt="CoinSynch" width={21} height={21}/>
                     </a>
                 </div>
 
                 <div className="flex items-center justify-start w-1/5 p-5">
                     <nav className="flex items-center self-start justify-start w-full max-w-5xl">
-                        <a href="/" className="w-1/2 hidden text-sm text-gray-400 dark:text-gray-100 hover:text-gray-700 dark:hover:text-gray-300 lg:block">About Us</a>
-                        <a href="/" className="w-1/2 hidden text-sm text-gray-400 dark:text-gray-100 hover:text-gray-700 dark:hover:text-gray-300 lg:block">Top Cryptos</a>
+                        <a href="/"
+                           className="w-1/2 hidden text-sm text-gray-400 dark:text-gray-100 hover:text-gray-700 dark:hover:text-gray-300 lg:block">About
+                            Us</a>
+                        <a href="/"
+                           className="w-1/2 hidden text-sm text-gray-400 dark:text-gray-100 hover:text-gray-700 dark:hover:text-gray-300 lg:block">Top
+                            Cryptos</a>
                     </nav>
                 </div>
 
 
-                <div className='flex items-center'>
-
+                <div className='flex items-center w-1/4'>
+                    <div className="flex items-center gap-1 w-1/2">
+                        <p>SOL</p>
+                        <span className='text-gray-400'>R$ {solanaPrice.toFixed(2)}</span>
+                    </div>
+                    <div className="flex items-center gap-1  w-1/2">
+                        <p>XLM</p>
+                        <span className='text-gray-400'>R$ {xlmPrice.toFixed(2)}</span>
+                    </div>
                 </div>
 
-                <div className="flex items-center justify-between w-1/3 p-5">
-                    <button href="/" className="w-1/2 hidden text-sm font-medium text-gray-900 dark:text-gray-100 hover:text-gray-700 dark:hover:text-gray-300 lg:block">Sign in</button>
-                    <button href="/" className="w-1/2 p-2 hidden text-sm font-medium text-white bg-yellow-500 rounded-2xl lg:block">Sign Up</button>
+                <div className="flex items-center justify-between w-1/5">
+                    <button href="/"
+                            className="w-1/2 hidden text-sm font-medium text-gray-900 dark:text-gray-100 hover:text-gray-700 dark:hover:text-gray-300 lg:block">Sign
+                        in
+                    </button>
+                    <button href="/"
+                            className="w-1/2 p-2 hidden text-sm font-medium text-white bg-yellow-500 rounded-2xl lg:block">Sign
+                        Up
+                    </button>
                 </div>
 
             </div>
