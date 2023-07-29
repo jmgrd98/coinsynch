094eb441a42db8f5207bd3dcde30bcc3f6f36b4d
João Marcelo Dantas <jmgrd98@gmail.com>
2023-07-29 13:48:23 -0300

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,6 +8,7 @@ import TopCryptos from "@/components/TopCryptos";
 import Newsletter from "@/components/Newsletter";
 import Sidebar from "@/components/Sidebar";
 import balanceIcon from '../../public/assets/icons/balance-icon.svg'
+import walletIcon from '../../public/assets/icons/wallet-icon.svg'
 
 export default function Dashboard() {
 
@@ -16,28 +17,45 @@ export default function Dashboard() {
 
     return (
         <section className='flex'>
-            <Sidebar className='w-1/4'/>
+            <Sidebar className='w-1/4 '/>
 
-            <main className='bg-gray-100 w-full p-10'>
+            <main className='bg-gray-100 w-full p-10 '>
 
                 <section className='flex gap-10'>
-                    <section className='bg-white w-1/3 rounded shadow flex gap-3'>
-                        <div className='p-5'>
+                    <section className='bg-white w-1/2 rounded shadow flex justify-between'>
+                        <div className='p-5 flex gap-3'>
                             <Image src={balanceIcon} alt='Balance icon' width={65} height={65}
                                    className='bg-yellow-100 rounded-3xl p-3'/>
                             <div className='flex flex-col gap-2'>
-                                <h3 className='text-gray-500 text-2xl'>Balance in US$</h3>
+                                <h3 className='text-gray-500 text-xl'>Balance in US$</h3>
                                 <p className='text-gray-300'>(approximately)</p>
                             </div>
                         </div>
 
-                        <div className='bg-yellow-100'>
-                            <h3 className='text-gray-700 text-2xl'>US$ {balance}</h3>
+                        <div className='bg-yellow-100 font-bold text-gray-700 text-xl p-3 w-1/3 flex items-center justify-center'>
+                            $ {balance}
                         </div>
                     </section>
-                    
-                    <section className='bg-white w-1/3 p-5 rounded shadow'></section>
-                    <section className='bg-white w-1/3 p-5 rounded shadow'></section>
+
+                    <section className='bg-white w-1/4 p-5 rounded shadow'>
+                        <p className='text-gray-500'>Daily variation</p>
+
+                    </section>
+                    <section className='bg-white w-1/4 p-5 rounded shadow'>
+                        <div className='flex flex-col gap-2'>
+                        <p className='text-gray-500 font-bold'>NFT's NEWS</p>
+                            <p className='text-gray-300'>New ElephantX NFT to be launched!</p>
+                            <p className='text-yellow-400'>Read more +</p>
+                        </div>
+                    </section>
+                </section>
+
+
+                <section className='flex p-5 bg-white mt-10 rounded-2xl'>
+                    <div className='flex gap-3 items-center'>
+                    <Image src={walletIcon} alt='Wallet icon' width={50} height={50} />
+                    <h2 className='text-2xl font-bold text-gray-500'>My Wallet</h2>
+                    </div>
                 </section>
             </main>
 
