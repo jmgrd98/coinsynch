23ca70e1378f5d60ace315322ed47ee1e7a0240f
João Marcelo Dantas <jmgrd98@gmail.com>
2023-07-29 15:21:00 -0300

    dashboard empty state

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,6 +9,8 @@ import Newsletter from "@/components/Newsletter";
 import Sidebar from "@/components/Sidebar";
 import balanceIcon from '../../public/assets/icons/balance-icon.svg'
 import walletIcon from '../../public/assets/icons/wallet-icon.svg'
+import elephantx from '../../public/assets/images/elephantx.svg'
+import grayWalletIcon from '../../public/assets/icons/gray-wallet.svg'
 
 export default function Dashboard() {
 
@@ -32,7 +34,8 @@ export default function Dashboard() {
                             </div>
                         </div>
 
-                        <div className='bg-yellow-100 font-bold text-gray-700 text-xl p-3 w-1/3 flex items-center justify-center'>
+                        <div
+                            className='bg-yellow-100 font-bold text-gray-700 text-xl p-3 w-1/3 flex items-center justify-center'>
                             $ {balance}
                         </div>
                     </section>
@@ -41,21 +44,42 @@ export default function Dashboard() {
                         <p className='text-gray-500'>Daily variation</p>
 
                     </section>
-                    <section className='bg-white w-1/4 p-5 rounded shadow'>
-                        <div className='flex flex-col gap-2'>
-                        <p className='text-gray-500 font-bold'>NFT's NEWS</p>
+                    <section className='bg-white w-1/4 flex rounded shadow'>
+                        <div className='flex p-3 flex-col gap-2'>
+                            <p className='text-gray-500 font-bold'>NFT's NEWS</p>
                             <p className='text-gray-300'>New ElephantX NFT to be launched!</p>
                             <p className='text-yellow-400'>Read more +</p>
                         </div>
+
+                        <div>
+                            <Image src={elephantx} alt='ElephantX NFT image' width={200} height={200}/>
+                        </div>
                     </section>
+
                 </section>
 
 
-                <section className='flex p-5 bg-white mt-10 rounded-2xl'>
-                    <div className='flex gap-3 items-center'>
-                    <Image src={walletIcon} alt='Wallet icon' width={50} height={50} />
-                    <h2 className='text-2xl font-bold text-gray-500'>My Wallet</h2>
-                    </div>
+                <section className='flex gap-10 mt-10 flex-col bg-white'>
+                    <section className='flex p-5 mt-10 rounded-2xl'>
+                        <div className='flex gap-3 items-center border-b p-3 border-b-gray-200 justify-between w-full'>
+                            <div className='flex gap-3 items-center'>
+                                <Image src={walletIcon} alt='Wallet icon' width={50} height={50}/>
+                                <h2 className='text-2xl font-bold text-gray-500'>My Wallet</h2>
+                            </div>
+
+                            <button className='bg-yellow-500 text-white rounded-3xl p-2'>+ Add Crypto</button>
+                        </div>
+                    </section>
+
+                    <section className='flex flex-col gap-5 m-auto p-10 items-center text-center'>
+                        <div className='bg-gray-100 w-1/2 p-5 rounded-3xl flex gap-5'>
+                            <Image src={grayWalletIcon} alt='Empty wallet icon' width={100} height={100}/>
+                        </div>
+
+                        <h3 className='text-gray-500 font-bold text-2xl'>Nothing here yet...</h3>
+                        <p className='text-gray-500'>Add a crypto and start earning.</p>
+                    </section>
+
                 </section>
             </main>
 
