1db6fef99cbc07b0a819172476d027179269e995
João Marcelo Dantas <jmgrd98@gmail.com>
2023-07-29 16:47:13 -0300

    Update AddCryptoModal.tsx

--- components/AddCryptoModal.tsx
+++ components/AddCryptoModal.tsx
@@ -4,10 +4,34 @@ import {useState} from 'react';
 
 export default function AddCryptoModal({onClose}: { onClose: () => void }) {
 
+    const [selectedCrypto, setSelectedCrypto] = useState('');
+    const [quantity, setQuantity] = useState(0);
+
     function handleOverlayClick(e) {
         if (e.target.classList.contains('modal-overlay')) {
             onClose();
         }
+    }
+
+    function handleSelect(e) {
+        setSelectedCrypto(e.target.value);
+    }
+
+    function handleChange(e) {
+        setQuantity(e.target.value);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        if (selectedCrypto === '') {
+            console.log('Please select a crypto');
+            return;
+        }
+
+        console.log('Crypto added successfully');
+        console.log(selectedCrypto, quantity);
+        onClose();
     }
 
     return (
@@ -17,15 +41,20 @@ export default function AddCryptoModal({onClose}: { onClose: () => void }) {
                 onClick={handleOverlayClick}
             ></div>
 
-            <form className='fixed z-20 flex flex-col p-5 items-center rounded bg-white top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
-                <h2 className='text-2xl font-bold'>Add Crypto</h2>
-                <select className='bg-gray-100 p-2 rounded mt-5'>
-                    <option value="BTC">BTC</option>
-                    <option value="ETH">ETH</option>
-                    <option value="ADA">ADA</option>
-                    <option value="BNB">BNB</option>
-                    <option value="USDT">USDT</option>
+            <form className='fixed z-20 flex flex-col p-5 items-center justify-evenly rounded bg-white top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-1/4 h-1/2'>
+                <h2 className='text-2xl font-bold text-gray-500'>Add Crypto</h2>
+                <select className='bg-gray-100 p-2 rounded mt-5 w-full' onChange={handleSelect}>
+                    <option value="Select" className='text-gray-500'>Choose Crypto</option>
+                    <option value="BTC" className='text-gray-500'>BTC</option>
+                    <option value="ETH" className='text-gray-500'>ETH</option>
+                    <option value="ADA" className='text-gray-500'>ADA</option>
+                    <option value="BNB" className='text-gray-500'>BNB</option>
+                    <option value="USDT" className='text-gray-500'>USDT</option>
                 </select>
+
+                <input type={'number'} placeholder={'0.0'} className='bg-gray-100 p-2 rounded mt-5 w-full' onChange={handleChange} />
+
+                <button className='bg-yellow-500 text-white p-2 rounded-3xl mt-5 w-full' onClick={handleSubmit}>Add Crypto</button>
             </form>
         </>
     );
